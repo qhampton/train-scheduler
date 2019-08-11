f22986c6c4ed3c564b1597ca200d5e2b8148e84e
Quinlan Hampton <qnhampton93@gmail.com>
2019-08-10 23:47:47 -0700

    added complete html and css format and styling, updated images in folder, commented js file for coding

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+//Firebase Confgis and Initialize
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDHyjcJRFLTkbRv4D6vqiwDjrYiafjBxxQ",
+    authDomain: "hw-7-73128.firebaseapp.com",
+    databaseURL: "https://hw-7-73128.firebaseio.com",
+    projectId: "hw-7-73128",
+    storageBucket: "",
+    messagingSenderId: "665026269753",
+    appId: "1:665026269753:web:a9b287d5bdd238e1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//adding employees once submit button
+
+    //grabs user input
+
+    //stores locally temporarily
+
+    //uploads info to databse
+
+    //check console log
+
+    //clear inputs for next add
+
+//create events in Firebase every entry
+
+    //create new row in table
+
+    //push to table
